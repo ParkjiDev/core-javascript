5666b69b4fcf9531acb8d6e9454d4cf082cded11
ParkjiDev <parkjidev@gmail.com>
2023-12-04 16:37:15 +0900

    [learn] basic 학습 - callback function - IIFE (즉시실행함수)

--- client/chapter/core/11-2.function.js
+++ client/chapter/core/11-2.function.js
@@ -58,20 +58,147 @@ let calculateTotal = function () {
   return total;
 };
 
+// forEach : 값을 반환하지 않음
+// reduce : 값을 반환함
+// map : 값을 반환함. 단, 배열만 반환
+// filter : 값을 반환함. 단, 배열만 반환
+
+const arr = ['김다영', '이경화', '김용범'];
+// const newArr = ['김다영-hi','이경화-hi','김용범-hi'];
+
+// let name = [];
+
+// arr.forEach(function(item){
+//   name.push(item + '-hi')
+// })
+
+// const name = arr.reduce(function(acc,cur){
+//   acc.push(cur + '-hi');
+//   return acc;
+// },[])
+
+// console.log( name );
+const name = arr.map(function (item) {
+  return item + '-hi';
+});
+
+/* -------------------------------------------------------------------------- */
 const result = calculateTotal(10, 20, 30, 40, 50, 60);
 console.log('result', result);
 
 // 익명(이름이 없는) 함수 (표현)식
-let anonymousFunctionExpression;
+let anonymousFunctionExpression = function () {};
 
 // 유명(이름을 가진) 함수 (표현)식
-let namedFunctionExpression;
+let namedFunctionExpression = function hello() {};
 
 // 콜백 함수 (표현)식
-let callbackFunctionExpression;
+let cb = function (isActive, success, fail) {
+  if (isActive) {
+    success();
+  } else {
+    fail();
+  }
+};
+
+cb(
+  true,
+  function () {
+    console.log('성공');
+  },
+  function () {
+    console.log('실패');
+  }
+);
+/* -------------------------------------------------------------------------- */
+function movePage(url, success, fail) {
+  if (url.match(/http.+www/) || typeof url === 'string') {
+    success(url);
+  } else {
+    fail(url);
+  }
+}
+
+movePage(
+  'www.naver.com',
+  function (url) {
+    console.log('해당 페이지로 넘어갑니다.');
+  },
+  function (url) {
+    console.log('입력하신 url 주소는 없는 주소입니다.');
+  }
+);
 
 // 함수 선언문 vs. 함수 (표현)식
 
 // 즉시 실행 함수 (표현)식
 // Immediately Invoked Function Expression
+
 let IIFE;
+
+// 함수가 선언 됨과 동시에 실행되는 것을 말합니다.
+
+// 📂
+// import css from 'css.js' // module
+
+// var는 블록 스코프 : x
+// var는 함수 스코프 : o
+
+// incapsulation (캡슐화)
+// 모듈화
+
+// const MASTER = (function (g){
+
+//   console.log( g.alert() );
+//   let uuid =  'askdjazxjd!@#!@$123';
+
+//   return {
+//     getKey(){
+//       return uuid
+//     },
+//     setKey(value){
+//       uuid = value
+//     }
+//   }
+
+// })(window);
+
+// // console.log(MASTER.getKey());
+
+// MASTER.setKey('새로운 비밀번호')
+
+const css = (function () {
+  function getStyle(node, prop) {
+    if (typeof node === 'string') node = document.querySelector(node);
+    if (typeof prop !== 'string')
+      throw new Error(
+        'getStyle 함수의 두 번째 인수는 string 타입 이어야 합니다.'
+      );
+
+    return getComputedStyle(node, null)[prop];
+  }
+
+  function setStyle(node, prop, value) {
+    if (typeof node === 'string') node = document.querySelector(node);
+    if (typeof prop !== 'string')
+      throw new Error(
+        'setStyle 함수의 두 번째 인수는 string 타입 이어야 합니다.'
+      );
+    if (!value)
+      throw new Error('setStyle 함수의 세 번째 인수는 필수값 입니다.');
+
+    node.style[prop] = value;
+  }
+
+  function css(node, prop, value) {
+    // if(!value){
+    //   return getStyle(node,prop) // getter
+    // }else{
+    //   setStyle(node,prop,value) // setter
+    // }
+
+    return !value ? getStyle(node, prop) : setStyle(node, prop, value);
+  }
+
+  return css;
+})();
